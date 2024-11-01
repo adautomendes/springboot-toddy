57ae1b830ab58f13651718df709aebcf413b4cd6
Adauto Mendes <adauto.inatel@gmail.com>
2024-11-01 08:46:55 -0300

    Add full pipeline

---
+++ cicd/full.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                build job: "build", wait: true
+            }
+        }
+        stage('Test') {
+            steps {
+                build job: "test", wait: true, parameters: [
+                    string(name: 'toddySizeMin', value: "${toddySizeMin}"),
+                    string(name: 'toddySizeMax', value: "${toddySizeMax}")
+                ]
+            }
+        }
+        stage('Documentation') {
+            steps {
+                build job: "documentation", wait: true
+            }
+        }
+    }
+}
